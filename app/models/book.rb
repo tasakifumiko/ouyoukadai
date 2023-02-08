bdce3c0e7f1c1ff59ae0b97c584cee72dd63222a
TasakiFumiko <121810685+tasakifumiko@users.noreply.github.com>
2023-02-08 02:27:14 +0000

    7a

--- app/models/book.rb
+++ app/models/book.rb
@@ -10,7 +10,7 @@ class Book < ApplicationRecord
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
-end
+
 
 # 検索方法分岐
   def self.looks(search, word)
@@ -26,3 +26,11 @@ end
       @book = Book.all
     end
   end
+
+#本の投稿数を数える
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) } # 今日
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) } # 前日
+  scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) } 
+  scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) } 
+
+end
