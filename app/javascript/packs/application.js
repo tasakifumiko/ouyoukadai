264a9b6813da49dfd49dc0733142976c663a8ea2
TasakiFumiko <121810685+tasakifumiko@users.noreply.github.com>
2023-02-08 07:48:12 +0000

    8b

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+//= import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "jquery"
@@ -13,6 +13,10 @@ import "bootstrap"
 import '@fortawesome/fontawesome-free/js/all';
 import "../stylesheets/application" 
 
+import "chartkick"
+import "chart.js"
+
+
 Rails.start()
-Turbolinks.start()
+//Turbolinks.start()
 ActiveStorage.start()
